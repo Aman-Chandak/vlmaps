962ce21cd02c62201e971784b43ad58932e2ab66
Aman Chandak <98462425+Aman-Chandak@users.noreply.github.com>
2024-12-04 17:08:27 -0700

    Delete vlmaps directory

--- vlmaps/install.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt
-
-cd ~
-git clone --recursive https://github.com/cvg/Hierarchical-Localization/
-cd Hierarchical-Localization/
-python -m pip install -e .
